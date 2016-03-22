f6bf999321966f1c129cbbb16622e5b01dfb35d2
Brecht De Man <brechtdeman@hotmail.com>
2016-03-22 08:18:34 +0100

    Add K filter
    Calculated K filter from coefficients given in ITU-R BS.1770-4.

--- pycoustics/filters.py
+++ pycoustics/filters.py
@@ -29,3 +29,66 @@ def third_octave_filter(x, fc, fs, order=2):
     higher_freq = fc*sixth_root_of_2
     y = butter_cascade(x, lower_freq, higher_freq, fs, order)
     return y
+
+def K_filter(signal, fs, debug=False):
+    # apply K filtering as specified in EBU R-128 / ITU BS.1770-4
+
+    # pre-filter 1
+    f0 = 1681.9744509555319
+    G  = 3.99984385397
+    Q  = 0.7071752369554193
+    K  = np.tan(np.pi * f0 / fs) # TODO: precompute
+    Vh = np.power(10.0, G / 20.0)
+    Vb = np.power(Vh, 0.499666774155)
+    a0_ = 1.0 + K / Q + K * K
+    b0 = (Vh + Vb * K / Q + K * K) / a0_
+    b1 = 2.0 * (K * K -  Vh) / a0_
+    b2 = (Vh - Vb * K / Q + K * K) / a0_
+    a0 = 1.0
+    a1 = 2.0 * (K * K - 1.0) / a0_
+    a2 = (1.0 - K / Q + K * K) / a0_
+    signal_1 = lfilter([b0,b1,b2],[a0,a1,a2],signal)
+    
+    if debug:
+        plt.figure(figsize=(9,9))
+        ax1 = fig.add_subplot(111)
+        w, h1 = freqz([b0,b1,b2], [a0,a1,a2], worN=8000)#np.logspace(-4, 3, 2000))
+        plt.semilogx((fs * 0.5 / np.pi) * w, 20*np.log10(abs(h1)))
+        plt.title('Pre-filter 1')
+        plt.xlabel('Frequency [Hz]')
+        plt.ylabel('Gain [dB]')
+        plt.xlim([20, 20000])
+        plt.ylim([-10,10])
+        plt.grid(True, which='both')
+        ax = plt.axes()
+        ax.yaxis.set_major_locator(ticker.MultipleLocator(2))
+        plt.show()
+    
+    # pre-filter 2
+    f0 = 38.13547087613982
+    Q  =  0.5003270373253953
+    K  = np.tan(np.pi * f0 / fs)
+    a0 = 1.0
+    a1 = 2.0 * (K * K - 1.0) / (1.0 + K / Q + K * K)
+    a2 = (1.0 - K / Q + K * K) / (1.0 + K / Q + K * K)
+    b0 = 1.0
+    b1 = -2.0
+    b2 = 1.0
+    signal_2 = lfilter([b0,b1,b2],[a0,a1,a2],signal_1)
+    
+    if debug:
+        plt.figure(figsize=(9,9))
+        ax1 = fig.add_subplot(111)
+        w, h2 = freqz([b0,b1,b2], [a0,a1,a2], worN=8000)#np.logspace(-4, 3, 2000))
+        plt.semilogx((fs * 0.5 / np.pi) * w, 20*np.log10(abs(h2)))
+        plt.title('Pre-filter 2')
+        plt.xlabel('Frequency [Hz]')
+        plt.ylabel('Gain [dB]')
+        plt.xlim([10, 20000])
+        plt.ylim([-30,5])
+        plt.grid(True, which='both')
+        ax = plt.axes()
+        ax.yaxis.set_major_locator(ticker.MultipleLocator(5))
+        plt.show()
+    
+    return signal_2 # return signal passed through 2 pre-filters
