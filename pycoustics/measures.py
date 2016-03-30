2dfae7921de93ed22391c8107bce84ca7ad699c3
Brecht De Man <brechtdeman@hotmail.com>
2016-03-30 09:36:49 +0200

    Added loudness calculation

--- pycoustics/measures.py
+++ pycoustics/measures.py
@@ -27,3 +27,46 @@ def TS(impulse_response, fs, delay=0): # centre time
     total_energy = np.sum(np.square(impulse_response)) # ∑ p^2(t)
     return 1000.0*numerator/total_energy # expressed in ms
     
+def calculate_loudness(signal,fs):
+    # filter
+    if len(signal.shape)==1: # if shape (N,), then make (N,1)
+        signal_filtered = copy.copy(signal.reshape((signal.shape[0],1)))
+    else:
+        signal_filtered = copy.copy(signal)
+        
+    for i in range(signal_filtered.shape[1]):
+        signal_filtered[:,i] = K_filter(signal_filtered[:,i], fs)
+
+    # mean square
+    G = [1.0, 1.0, 1.0, 1.41, 1.41]
+    T_g = 0.400 # 400 ms gating block
+    Gamma_a = -70.0 # absolute threshold: -70 LKFS
+    overlap = .75 # relative overlap (0.0-1.0)
+    step = 1 - overlap
+
+    T = signal_filtered.shape[0]/fs # length of measurement interval in seconds
+    j_range = np.arange(0,(T-T_g)/(T_g*step))
+    z = np.ndarray(shape=(signal_filtered.shape[1],len(j_range))) # ?
+    # write in explicit for-loops for readability and translatability
+    for i in range(signal_filtered.shape[1]): # for each channel i
+        for j in j_range: # for each window j
+            lbound = np.round(fs*T_g*j*step).astype(int)
+            hbound = np.round(fs*T_g*(j*step+1)).astype(int)
+            z[i,j] = (1/(T_g*fs))*np.sum(np.square(signal_filtered[lbound:hbound, i]))
+
+    G_current = np.array(G[:signal_filtered.shape[1]]) # discard weighting coefficients G_i unused channels
+    n_channels = G_current.shape[0]
+    l = [-.691 + 10.0*np.log10(np.sum([G_current[i]*z[i,j.astype(int)] for i in range(n_channels)])) \
+             for j in j_range]
+    #print 'l: '+str(l)
+
+    # throw out anything below absolute threshold:
+    indices_gated = [idx for idx,el in enumerate(l) if el > Gamma_a] 
+    z_avg = [np.mean([z[i,j] for j in indices_gated]) for i in range(n_channels)]
+    Gamma_r = -.691 + 10.0*np.log10(np.sum([G_current[i]*z_avg[i] for i in range(n_channels)])) - 10.0
+    # throw out anything below relative threshold:
+    indices_gated = [idx for idx,el in enumerate(l) if el > Gamma_r] 
+    z_avg = [np.mean([z[i,j] for j in indices_gated]) for i in range(n_channels)]
+    L_KG = -.691 + 10.0*np.log10(np.sum([G_current[i]*z_avg[i] for i in range(n_channels)]))
+
+    return L_KG
